4ac78ba39340b7cc095e44ce193fc83e7eeeed92
IvanGabrielG <111589109+IvanGabrielG@users.noreply.github.com>
2023-02-01 00:15:03 -0300

    Añadidos archivos de CV
    Se añadieron los archivos necesarios para ver el CV.

---
+++ index-java.js
@@ -0,0 +1,26 @@
+function mostrar(){
+    document.getElementById('info').style.display = 'block';
+}
+
+function ocultar(){
+    document.getElementById('info').style.display = 'none';
+}
+
+function mostrar1(){
+    document.getElementById('info1').style.display = 'block';
+}
+
+function ocultar1(){
+    document.getElementById('info1').style.display = 'none';
+}
+
+function mostrar2(){
+    document.getElementById('info2').style.display = 'block';
+}
+
+function ocultar2(){
+    document.getElementById('info2').style.display = 'none';
+}
+/*function mostrar(){
+    document.getElementsByClassName('info').style.display = 'block';
+}*/
